daf695d23a80f8a9e6d077d0402b667165e4a685
Oluwafemi <oluwafemiomolounnu@gmail.com>
2022-08-25 15:19:11 +0100

    again

--- learn.js
+++ learn.js
@@ -17,6 +17,7 @@
 // var name = prompt("What is your name?");
 // var getFirst = name.slice(0,1);
 // var capFirst = getFirst.toUpperCase();
-//  var otherLetters = name.slice(1, name.length);
+// var otherLetters = name.slice(1, name.length);
+// otherLetters = otherLetters.toLowerCase();
 // var fullName = capFirst + otherLetters;
 // alert("Hello " + fullName);
