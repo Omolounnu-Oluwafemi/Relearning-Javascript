a093ed0d93d038667f288264434f511718d9ce9d
Oluwafemi <oluwafemiomolounnu@gmail.com>
2022-08-25 14:25:41 +0100

    uppercase and lowercase

--- learn.js
+++ learn.js
@@ -1,7 +1,12 @@
 // STRING .lenght() AND .slice()
-var tweet = prompt("compose your tweet:");
-var tweetDisplay = tweet.slice(0,10);
-alert(tweetDisplay);
-alert(tweetDisplay.length + " words")
+// var tweet = prompt("compose your tweet:");
+// var tweetDisplay = tweet.slice(0,10);
+// alert(tweetDisplay);
+// alert(tweetDisplay.length + " words")
 
 // toUpperCase()
+var myName = prompt("What is your name?");
+alert("Hello " + myName.toUpperCase());
+// toLowerCase()
+var myName = prompt("What is your name?");
+alert("Hello " + myName.toLowerCase());
