3429be3db4dac13408f8ce0921a5cfe8c1c29c11
Oluwafemi <oluwafemiomolounnu@gmail.com>
2022-08-25 14:10:35 +0100

    I am learning Javascript

---
+++ learn.js
@@ -0,0 +1,7 @@
+// STRING .lenght() AND .slice()
+var tweet = prompt("compose your tweet:");
+var tweetDisplay = tweet.slice(0,10);
+alert(tweetDisplay);
+alert(tweetDisplay.length + " words")
+
+// toUpperCase()
