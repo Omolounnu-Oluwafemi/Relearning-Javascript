f0126ba8f89d8eabf2d9a37cb1c05743508868fe
Oluwafemi <oluwafemiomolounnu@gmail.com>
2022-08-25 15:09:55 +0100

    progress

--- learn.js
+++ learn.js
@@ -5,8 +5,18 @@
 // alert(tweetDisplay.length + " words")
 
 // toUpperCase()
-var myName = prompt("What is your name?");
-alert("Hello " + myName.toUpperCase());
-// toLowerCase()
-var myName = prompt("What is your name?");
-alert("Hello " + myName.toLowerCase());
+// var myName = prompt("What is your name?");
+// alert("Hello " + myName.toUpperCase());
+// // toLowerCase()
+// var myName = prompt("What is your name?");
+// alert("Hello " + myName.toLowerCase()); 
+
+// Example
+// Write a code asking the users for their names, then greet them using a name with only the first letter capitalized
+
+// var name = prompt("What is your name?");
+// var getFirst = name.slice(0,1);
+// var capFirst = getFirst.toUpperCase();
+//  var otherLetters = name.slice(1, name.length);
+// var fullName = capFirst + otherLetters;
+// alert("Hello " + fullName);
